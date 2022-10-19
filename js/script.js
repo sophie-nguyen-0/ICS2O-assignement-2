26298a52cb1afad21e0c716ac62ead9c89e51965
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-10-19 18:48:37 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@
 // Created on: oct 2020
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * Do basic math.
  */
 function calculate() {
-   // input
+  // input
   const sideA = parseInt(document.getElementById("side-a").value)
   const sideB = parseInt(document.getElementById("side-b").value)
   const sideC = parseInt(document.getElementById("side-c").value)
 
   // process
-  const perimeter =(sideA + sideB + sideC)
+  const perimeter = sideA + sideB + sideC
 
   // output
-  document.getElementById("perimeter").innerHTML = "<h5> the perimeter is: " + perimeter + " cm</h5>"
+  document.getElementById("perimeter").innerHTML =
+    "<h5> the perimeter is: " + perimeter + " cm</h5>"
 }
