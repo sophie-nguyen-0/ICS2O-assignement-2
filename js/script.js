02aa91fc4ee54160ebab2b96d04895465b5a3f99
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-19 18:46:34 +0000

    js and html

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Created by: sophie
+// Created on: oct 2020
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * Do basic math.
+ */
+function calculate() {
+   // input
+  const sideA = parseInt(document.getElementById("side-a").value)
+  const sideB = parseInt(document.getElementById("side-b").value)
+  const sideC = parseInt(document.getElementById("side-c").value)
+
+  // process
+  const perimeter =(sideA + sideB + sideC)
+
+  // output
+  document.getElementById("perimeter").innerHTML = "<h5> the perimeter is: " + perimeter + " cm</h5>"
+}
